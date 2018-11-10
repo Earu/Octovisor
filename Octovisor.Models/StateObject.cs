fe7e51009d3b4d0e83a980f412eda5b3dc1379d9
R.Réno <earu.derg@gmail.com>
2018-11-10 14:48:01 +0100

    [Server] Make the server client-proof

--- Octovisor.Models/StateObject.cs
+++ Octovisor.Models/StateObject.cs
@@ -1,21 +1,31 @@
-﻿using System.Net.Sockets;
+﻿using System;
+using System.Net.Sockets;
 using System.Text;
 
 namespace Octovisor.Models
 {
-    public class StateObject
+    public class StateObject : IDisposable
     {
         public const int BufferSize = 256;
 
         public Socket WorkSocket { get; }
         public byte[] Buffer { get; }
         public StringBuilder Builder { get; }
+        public string Identifier { get; set; }
+        public bool IsDisposed { get; private set; }
 
         public StateObject(Socket socket)
         {
+            this.IsDisposed = false;
             this.WorkSocket = socket;
             this.Buffer = new byte[BufferSize];
             this.Builder = new StringBuilder();
+        }
+
+        public void Dispose()
+        {
+            this.IsDisposed = true;
+            this.WorkSocket.Close();
         }
     }
 }
