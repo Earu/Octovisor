4330ccaffabbddc4575636d53ada632d976fdd53
Earu <earu.derg@gmail.com>
2019-04-13 12:56:00 +0200

    Use MessageSerializer, for possible future-proof serializer changes

---
+++ Octovisor.Messages/MessageSerializer.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Octovisor.Messages
+{
+    public static class MessageSerializer
+    {
+        public static string Serialize(object obj)
+            => JsonConvert.SerializeObject(obj);
+
+        public static T Deserialize<T>(string json)
+            => JsonConvert.DeserializeObject<T>(json);
+    }
+}
