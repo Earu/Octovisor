bafca6fa0d46a4b444bf939b40b612c58d3e17ba
Earu <earu.derg@gmail.com>
2019-04-19 01:29:29 +0200

    Minor improvements

--- Octovisor.Server/Servers/WebSocketServer.cs
+++ Octovisor.Server/Servers/WebSocketServer.cs
@@ -1,9 +1,17 @@
-﻿namespace Octovisor.Server.Servers
+﻿using System.Threading.Tasks;
+
+namespace Octovisor.Server.Servers
 {
     internal class WebSocketServer : BaseProtocolServer
     {
         public WebSocketServer(Logger logger, Dispatcher dispatcher) : base(logger, dispatcher)
         {
         }
+
+        internal override Task RunAsync()
+            => Task.CompletedTask;
+
+        internal override Task StopAsync()
+            => Task.CompletedTask;
     }
 }
