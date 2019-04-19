ddbae746b0d5573da2081c03ee8b1c82ec61ecb8
Earu <earu.derg@gmail.com>
2019-04-19 17:56:49 +0200

    websocket impl

--- Octovisor.Server/Servers/WebSocketServer.cs
+++ Octovisor.Server/Servers/WebSocketServer.cs
@@ -6,7 +6,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Octovisor.Server.Servers
@@ -87,6 +89,46 @@ namespace Octovisor.Server.Servers
 #pragma warning restore CS4014
         }
 
+        private string ComputeHandshakeHash(string data)
+        {
+            string match = new Regex("Sec-WebSocket-Key: (.*)").Match(data).Groups[1].Value.Trim();
+            byte[] bytes = Encoding.UTF8.GetBytes($"{match}258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+            return Convert.ToBase64String(SHA1.Create().ComputeHash(bytes));
+        }
+
+        private async Task DoHandshakeAsync(WebSocketClientState state)
+        {
+            if (state.HasHandshaked) return;
+
+            while (state.Client.Available < 3)
+                await Task.Delay(250);
+
+            byte[] buffer = new byte[state.Client.Available];
+            await state.Stream.ReadAsync(buffer, 0, buffer.Length);
+            string data = Encoding.UTF8.GetString(buffer);
+            if (data.StartsWith("GET"))
+            {
+                string eol = "\r\n";
+                string response = "HTTP/1.1 101 Switching Protocols" + eol
+                    + "Connection: Upgrade" + eol
+                    + "Upgrade: websocket" + eol
+                    + $"Sec-WebSocket-Accept: " + this.ComputeHandshakeHash(data) + eol + eol;
+                byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+                await state.Stream.WriteAsync(responseBytes);
+            }
+        }
+
+        private byte[] DecodeBytes(byte[] encoded)
+        {
+            byte[] decoded = new byte[encoded.Length];
+            byte[] mask = new byte[4] { 61, 84, 35, 6 };
+
+            for (int i = 0; i < encoded.Length; i++)
+                decoded[i] = (byte)(encoded[i] ^ mask[i % 4]);
+
+            return decoded;
+        }
+
         private async Task ListenAsync(WebSocketClientState state)
         {
             try
@@ -95,7 +137,7 @@ namespace Octovisor.Server.Servers
                 int bytesRead = await stream.ReadAsync(state.Buffer);
                 if (bytesRead <= 0) return;
 
-                string data = Encoding.UTF8.GetString(state.Buffer, 0, bytesRead);
+                string data = Encoding.UTF8.GetString(state.Buffer);
 
                 List<Message> msgs = state.Reader.Read(data);
                 state.ClearBuffer();
