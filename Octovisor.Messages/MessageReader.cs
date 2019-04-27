ef1999196e794a1e95d8a4360f29c65ea7f71393
Earu <earu.derg@gmail.com>
2019-04-28 01:02:06 +0200

    More work on debugger, helper methods for OctoClient

--- Octovisor.Messages/MessageReader.cs
+++ Octovisor.Messages/MessageReader.cs
@@ -6,8 +6,6 @@ namespace Octovisor.Messages
 {
     public class MessageReader
     {
-        public event Action<string> MessageParsed;
-
         // Number of bytes possible before clearing up data (1GB)
         private const int Threshold = 1000000000; 
 
@@ -40,10 +38,7 @@ namespace Octovisor.Messages
                 {
                     string smsg = current.Substring(0, current.Length - 1);
                     if (!string.IsNullOrWhiteSpace(smsg))
-                    {
-                        this.MessageParsed?.Invoke(smsg);
                         msgs.Add(Message.Deserialize(smsg));
-                    }
 
                     this.Clear();
                 }
